5df64b73b3058d7ae9f5d25ad2812a38564ffa22
Sebi2202 <sebastian.herberth@yahoo.com>
2017-11-28 00:59:25 +0100

    UPDATE Timer added

--- CubaCountdown/javascript/quiz_seite.js
+++ CubaCountdown/javascript/quiz_seite.js
@@ -86,4 +86,21 @@ var quiz = new quiz(questions);
 
 populate();
 
+var total_seconds = 60*2;
+var c_minutes = parseInt(total_seconds/60);
+var c_seconds = parseInt(total_seconds%60);
+
+function checkTime() {
+	document.getElementById("quiz-time-left").innerHTML = 'Zeit übrig: ' + c_minutes + 'm:' + c_seconds + 's';
+	if(total_seconds <= 0) {
+		showScore();
+	}
+	else {
+		total_seconds = total_seconds - 1;
+		c_minutes = parseInt(total_seconds/60);
+		c_seconds = parseInt(total_seconds%60);
+		setTimeout("checkTime()", 1000);
+	}
+}
+setTimeout("checkTime()", 1000);
 
